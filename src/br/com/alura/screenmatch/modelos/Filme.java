334545eecc9ffd92f717260dcccf2ce41b5d0e66
hqaraujo <leopoldopet726@gmail.com>
2024-08-22 19:08:53 -0300

    Subindo projeto

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculo.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String diretor;
+
+    public String getDiretor() {
+
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return 0;
+    }
+}
+
+
